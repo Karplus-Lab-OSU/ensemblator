e75f46f1191c2f3ad0adbbb36a6ec1d518884ad5
Andrew E Brereton <andrew@brereton.me>
2016-07-29 13:15:00 -0700

    Add files via upload

--- ensemblator.py
+++ ensemblator.py
@@ -2871,6 +2871,16 @@ def analyze(options):
         except:
             pass
 
+        try:
+            mean_sil = {}
+            for resid in range(min(resid_list), (max(resid_list) + 1)):
+                try:
+                    mean_sil[resid] = (backbone_sil[resid] + eelocal_dict["lodr_sil"][resid]) / 2
+                except:
+                    mean_sil[resid] = None
+        except:
+            pass
+
 
         title = "eeGLOBAL_dcut=" + str(dcut)
         plt.figure()
@@ -2977,7 +2987,8 @@ def analyze(options):
             plt.plot(backbone_sil.keys(),
                      backbone_sil.values(),
                      blue,
-                     label="Global Silhouette Scores",
+                     alpha=0.4,
+                     label="Global Silhouette Score",
                      linewidth=1.5)
         except:
             pass   
@@ -2985,11 +2996,20 @@ def analyze(options):
             plt.plot(eelocal_dict["lodr_sil"].keys(),
                      eelocal_dict["lodr_sil"].values(),
                      green,
-                     label="LODR Silhouette Scores",
+                     alpha=0.4,
+                     label="LODR Silhouette Score",
                      linewidth=1.5)
         except:
             pass  
-                                       
+        try:
+            plt.plot(mean_sil.keys(),
+                     mean_sil.values(),
+                     red,
+                     label="Mean Silhouette Score",
+                     linewidth=1.5)
+        except:
+            pass
+                                           
         plt.xlabel("Residue Number")
         plt.ylabel("Backbone Silhouette Score")
         plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
@@ -3988,7 +4008,15 @@ def analyze(options):
                         backbone_sil[resid] = None
             except:
                 pass
-
+            try:
+                mean_sil = {}
+                for resid in range(min(resid_list), (max(resid_list) + 1)):
+                    try:
+                        mean_sil[resid] = (backbone_sil[resid] + eelocal_dict["lodr_sil"][resid]) / 2
+                    except:
+                        mean_sil[resid] = None
+            except:
+                pass
             title = "eeGLOBAL_dcut=" + str(dcut) + outputname
             plt.figure()
             try:
@@ -4106,7 +4134,8 @@ def analyze(options):
                 plt.plot(backbone_sil.keys(),
                          backbone_sil.values(),
                          blue,
-                         label="Global Silhouette Scores",
+                         alpha=0.4,
+                         label="Global Silhouette Score",
                          linewidth=1.5)
             except:
                 pass   
@@ -4114,11 +4143,19 @@ def analyze(options):
                 plt.plot(eelocal_dict["lodr_sil"].keys(),
                          eelocal_dict["lodr_sil"].values(),
                          green,
-                         label="LODR Silhouette Scores",
+                         alpha=0.4,
+                         label="LODR Silhouette Score",
                          linewidth=1.5)
             except:
                 pass  
-                                           
+            try:
+                plt.plot(mean_sil.keys(),
+                         mean_sil.values(),
+                         red,
+                         label="Mean Silhouette Score",
+                         linewidth=1.5)
+            except:
+                pass                                            
             plt.xlabel("Residue Number")
             plt.ylabel("Backbone Silhouette Score")
             plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),
